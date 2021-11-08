206cf3a03d65464fcc0150ee10231ad7e3e7d970
clp0072 <89806943+clp0072@users.noreply.github.com>
2021-11-08 09:32:10 -0600

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -8,14 +8,26 @@ class Temperature {
 		       degreesFahrenheit,
 		       degreesCelsius;
 	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		void setTempKelvin(double dk) {degreesKelvin = dk;}
+		double getTempKelvin() {return degreesKelvin;}
+		void setTempCelsius() {degreesCelsius  = degreesKelvin - 273.15;}
+		double getTempCelsius() {return degreesCelsius;} 
+		void setTempFahrenheit() {degreesFahrenheit = (getTempCelsius()/(5.0/9))+32;}
+		double getTempFahrenheit() {return degreesFahrenheit;}
 };
 
 int main() {
+	double dk;
+	Temperature temp;
+
+	cout << "Enter a temperautre in degrees Kelvin: ";
+	cin >> dk;
+
+	temp.setTempKelvin(dk);
+	temp.setTempCelsius();
+	temp.setTempFahrenheit();
+
+	cout << "Temperature in Celsius = " << temp.getTempCelsius() << endl;
+	cout << "Temperature in Fahrenheit = " << temp.getTempFahrenheit() << endl;
 
 }
